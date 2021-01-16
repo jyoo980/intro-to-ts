ea8213aa72c422df84507eb51e9188ff7072067b
James Yoo <jyoo980@gmail.com>
2021-01-15 20:11:42 -0800

    adding additional solution in array-starter.js

--- arrays/array-starter.js
+++ arrays/array-starter.js
@@ -4,7 +4,13 @@
 const nums = [1, 2, 3, 4];
 
 function doubleElements(arr) {
-    // TODO: return an array where every element of arr has been doubled.
+    // My preferred way of working with arrays
+    return arr.map(n => n * 2);
+    // let doubled = [];
+    // for (const n of arr) {
+    //     doubled.push(n * 2);
+    // }
+    // return doubled;
 }
 
 console.log(`Doubling: ${nums} resulted in: ${doubleElements(nums)}`);
@@ -12,7 +18,12 @@ console.log(`Doubling: ${nums} resulted in: ${doubleElements(nums)}`);
 // The next code doesn't appear in the video but it's still good practice
 
 function removeLessThanN(arr, n) {
-    // TODO: given an array of numbers and a number n, return only the values in
-    //       the array that are greater than n.
+    return arr.filter(num => num > n);
+    // let doubled = [];
+    // for (const element of arr) {
+    //     if (element > n) {
+    //         doubled.push(element);
+    //     }
+    // }
+    // return doubled;
 }
-
